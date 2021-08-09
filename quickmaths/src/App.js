85527b4a21ff2d9df59fad46f17edfa92b5426fa
Charlie-Eeles <41295345+Charlie-Eeles@users.noreply.github.com>
2021-08-09 02:02:00 +0100

    added functioning timer

--- quickmaths/src/App.js
+++ quickmaths/src/App.js
@@ -4,22 +4,34 @@ import React, { Component } from "react";
 class App extends Component{
 
   state = {
-    test: 0
+    interval: 1,
+    secondCount: 1
+  }
+  async componentDidMount(){
   }
 
   render(){
 
     const mathFunction = (x, clear) =>{
       const chosenTable = x || 16;
-      let secondCounter = 0;
       let scoreCounter = 0;
+      this.setState({secondCount: 5})
+      this.setState({interval : setInterval(()=> {this.setState({secondCount: this.state.secondCount-1})}, 1000)})
+    }
+
+    const clearIntervalFunc = () =>{
+      if(this.state.secondCount<=0){
+        clearInterval(this.state.interval)
+      }
     }
 
     return (
       
       <div>
         <button onClick={()=>{mathFunction()}}>Click here to start timer</button>
-        {this.state.test}
+        <button onClick={()=>{this.setState({secondCount: this.state.secondCount+1})}}>increase</button>
+        {clearIntervalFunc()}
+        {this.state.secondCount}
       </div>
     );
   }
